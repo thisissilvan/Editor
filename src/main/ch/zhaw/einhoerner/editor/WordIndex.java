4019b0ade2678693dd1b22a8b7e9d9bf1a28d070
Kunsang Kundetsang <kuendkun@students.zhaw.ch>
2019-11-06 21:19:10 +0100

    Class: Wordindex
    new method WordIndex
    -creates an index for each word

--- src/main/ch/zhaw/einhoerner/editor/WordIndex.java
+++ src/main/ch/zhaw/einhoerner/editor/WordIndex.java
@@ -1,4 +1,40 @@
 package ch.zhaw.einhoerner.editor;
 
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class WordIndex {
+    private Map<String, List<String>> woerterHaeufigkeit = new HashMap<>();
+    private static final int AMOUNT = 0;
+    private static final int PLACE = 1;
+    public WordIndex(List<String> text) {
+        for (int index = 0; index < text.size(); index++) {
+            String platzhalter = text.get(index);
+            String[] paragraph = platzhalter.toLowerCase().replaceAll("[ +.,?!\":; ]", " ").split(" +");
+
+            for (String word : paragraph) {
+                List<String> values = new ArrayList<>();
+                if (!woerterHaeufigkeit.containsKey(word)) {
+                    values.add(AMOUNT, "1");
+                    values.add(PLACE, String.valueOf(index));
+                    woerterHaeufigkeit.put(word, values);
+                } else {
+                    values = woerterHaeufigkeit.get(word);
+                    int raiseCount = Integer.parseInt(values.get(AMOUNT));
+                    raiseCount++;
+                    values.set(AMOUNT, String.valueOf(raiseCount));
+                    String newIndex = String.valueOf(index);
+                    if (!values.get(PLACE).contains(newIndex)) {
+                        String addPlace = values.get(PLACE);
+                        addPlace = addPlace + ", " + newIndex;
+                        values.set(PLACE, addPlace);
+                    }
+                    woerterHaeufigkeit.replace(word, values);
+                }
+
+            }
+        }
+    }
 }
