6cf89e1993248a77e8cba910776ecb337ccc6f7d
Kunsang Kundetsang <kuendkun@students.zhaw.ch>
2019-11-08 10:21:04 +0100

    Class: Wordindex
    refactoring of methods

--- src/main/ch/zhaw/einhoerner/editor/WordIndex.java
+++ src/main/ch/zhaw/einhoerner/editor/WordIndex.java
@@ -9,7 +9,13 @@ public class WordIndex {
     private Map<String, List<String>> woerterHaeufigkeit = new HashMap<>();
     private static final int AMOUNT = 0;
     private static final int PLACE = 1;
-    public WordIndex(List<String> text) {
+
+    public void WordIndex(List<String> text) {
+        makeWordIndex(text);
+        printWordIndex();
+    }
+
+    public void makeWordIndex(List<String> text) {
         for (int index = 0; index < text.size(); index++) {
             String platzhalter = text.get(index);
             String[] paragraph = platzhalter.toLowerCase().replaceAll("[ +.,?!\":; ]", " ").split(" +");
@@ -36,11 +42,34 @@ public class WordIndex {
 
             }
         }
+        entrysToDelete(controlMinimumAmount());
     }
+
+    private List<String> controlMinimumAmount(){
+        List<String> values;
+        List<String> toDelete = new ArrayList<>();
+        for (Map.Entry<String, List<String>> wortHaeufigkeit : woerterHaeufigkeit.entrySet()) {
+            values = wortHaeufigkeit.getValue();
+            int wordCount = Integer.parseInt(values.get(AMOUNT));
+            if (wordCount<3){
+                toDelete.add(wortHaeufigkeit.getKey());
+            }
+        }
+        return toDelete;
+    }
+
+    private void entrysToDelete(List<String> toDelete){
+        for (String keyToDelete: toDelete) {
+            woerterHaeufigkeit.remove(keyToDelete);
+        }
+
+    }
+
     public void printWordIndex() {
         for (Map.Entry<String, List<String>> wortHaeufigkeit : woerterHaeufigkeit.entrySet()) {
-            System.out.println("Wort:         "+ wortHaeufigkeit.getKey());
-            System.out.println("Paragraph/en: "+wortHaeufigkeit.getValue());
+            System.out.print(wortHaeufigkeit.getValue().get(AMOUNT));
+            System.out.print(" " + wortHaeufigkeit.getKey());
+            System.out.println(" : " + wortHaeufigkeit.getValue().get(PLACE));
         }
     }
 }
