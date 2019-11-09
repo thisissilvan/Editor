455995878e792e914ff11670c5b572e5d31c59e6
luethsil <luethsil@students.zhaw.ch>
2019-11-09 14:47:44 +0100

    add main to Editor.java

--- src/main/ch/zhaw/einhoerner/editor/Editor.java
+++ src/main/ch/zhaw/einhoerner/editor/Editor.java
@@ -1,4 +1,19 @@
 package ch.zhaw.einhoerner.editor;
 
 public class Editor {
+
+
+    public static void main(String[] args) {
+        {
+            Processor processor = new Processor();
+            processor.printWelcomeText();
+
+            boolean quit = false;
+            while (!quit) {
+               //todo do something
+            }
+            System.out.println("Thank you for using the Einhoerner Editor.");
+        }
+    }
+
 }
