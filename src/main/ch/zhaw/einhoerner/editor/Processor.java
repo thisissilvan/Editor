0ee8e62a80f4968f08fe9ecd345ffc8092f7deb2
luethsil <luethsil@students.zhaw.ch>
2019-11-09 14:52:40 +0100

    add methode printHelpText to class Processor

--- src/main/ch/zhaw/einhoerner/editor/Processor.java
+++ src/main/ch/zhaw/einhoerner/editor/Processor.java
@@ -15,9 +15,17 @@ public class Processor {
         System.out.println("Type in " + EnumCommand.HELP + " at any time for a short manual.");
         System.out.println();
         System.out.println("Please use one of the following comands to proceed:");
-        
+        printHelpText();
     }
 
+    public void printHelpText()
+    {
+        System.out.println();
+        System.out.println("You can choose from the following commands:");
+        System.out.println("add (with or without paragraph number), print, quit, help, searchAndReplace (followed by the old and the new word");
+        System.out.print("For a manual in detail, please use the Wiki in the Github repository.");
+        System.out.println();
+    }
 
 
 }
