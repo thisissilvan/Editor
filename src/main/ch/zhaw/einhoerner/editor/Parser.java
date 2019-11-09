159833663143fc1b4ce95610c096124f5073b6f8
hadiala1 <hadiala1@students.zhaw.ch>
2019-11-09 16:40:45 +0100

    implements Parser class: -created parseInput method

--- src/main/ch/zhaw/einhoerner/editor/Parser.java
+++ src/main/ch/zhaw/einhoerner/editor/Parser.java
@@ -1,4 +1,23 @@
 package ch.zhaw.einhoerner.editor;
 
 public class Parser {
+
+    /**
+     * @param input Enthält den Text, der auf Commands untersucht werden soll
+     * @return Command Liefert den erkannten Command zurück oder Command.UNKNOWN,
+     * falls kein gültiger Befehl gefunden werden konnte
+     */
+    public Command parseInput(String input) {
+        if (input.isEmpty()){
+            return Command.UNKNOWN;
+        }
+
+        for(Command command : Command.values()) {
+            if (input.startsWith(command.getCommand())) {
+                return command;
+            }
+        }
+
+        return Command.UNKNOWN;
+    }
 }
