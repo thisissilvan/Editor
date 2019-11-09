a342e3559f0803a57d6acf64dcdd8225001ec558
luethsil <luethsil@students.zhaw.ch>
2019-11-09 14:49:36 +0100

    add javadoc to main

--- src/main/ch/zhaw/einhoerner/editor/Editor.java
+++ src/main/ch/zhaw/einhoerner/editor/Editor.java
@@ -2,7 +2,14 @@ package ch.zhaw.einhoerner.editor;
 
 public class Editor {
 
-
+    /**
+     * The main methode runs until the user decides
+     * to quit the editor in a while loop.
+     * For a manual in detail, please consult the wiki page in the Github repository.
+     * @author Silvan Luethy
+     * @version 2019_11_09
+     *
+     */
     public static void main(String[] args) {
         {
             Processor processor = new Processor();
