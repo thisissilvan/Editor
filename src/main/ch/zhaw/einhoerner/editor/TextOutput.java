df34a75a98edc884cb3796740388e2b5033346d8
luethsil <luethsil@students.zhaw.ch>
2019-11-09 21:03:21 +0100

    add printFormatted methode

--- src/main/ch/zhaw/einhoerner/editor/TextOutput.java
+++ src/main/ch/zhaw/einhoerner/editor/TextOutput.java
@@ -23,6 +23,43 @@ public class TextOutput {
         }
     }
 
+    /**
+     * Print out a formatted version of all the paragraphs
+     * @param width - The maximum width of these paragraphs
+     */
 
-    
+    public void printFormatted(int width) {
+        List<String> allParagraphs = paragraphs.getParagraphs();
+
+        for (int i = 0; i < allParagraphs.size(); i++) {
+            String singleParagraph = formatParagraphWidth(allParagraphs.get(i), width);
+            System.out.println(singleParagraph);
+        }
+    }
+
+    /**
+     * Returns a single paragraph with the needed line breaks (\n) at the specific width nr
+     * @param paragraph - the paragraph to be formatted
+     * @param width     - the position at which a line break is added
+     * @return singleParagraph  - the formatted paragraph
+     */
+    public String formatParagraphWidth (String paragraph, int width){
+        List<String> paragraphList = new ArrayList<String>();
+        paragraphList.addAll(Arrays.asList(paragraph.split("")));
+
+        String singleParagraph = "";
+        width++;
+
+        for (int i = 0; i < paragraphList.size(); i++) {
+            if (i % width == 0) {
+                paragraphList.add(i, lineSeparator());
+            }
+            singleParagraph += paragraphList.get(i);
+        }
+
+        return singleParagraph;
+    }
+
+   
+
 }
