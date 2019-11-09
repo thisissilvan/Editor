2d8b9f76bfe2472599a88522e33319d3a7a3d1d7
luethsil <luethsil@students.zhaw.ch>
2019-11-09 18:20:11 +0100

    resolve conflicts in master branch

--- src/main/ch/zhaw/einhoerner/editor/Processor.java
+++ src/main/ch/zhaw/einhoerner/editor/Processor.java
@@ -44,7 +44,7 @@ public class Processor {
     {
         System.out.println();
         System.out.println("Welcome to the Editor Application from the team Einhoerner");
-        System.out.println("Type in " + EnumCommand.HELP + " at any time for a short manual.");
+        System.out.println("Type in " + Command.HELP + " at any time for a short manual.");
         System.out.println();
         System.out.println("Please use one of the following comands to proceed:");
         printHelpText();
--- src/test/ch/zhaw/einhoerner/editor/EnumCommandTest.java
+++
@@ -1,7 +0,0 @@
-package ch.zhaw.einhoerner.editor;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class EnumCommandTest {
-
-}
