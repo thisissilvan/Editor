d0ce7bdadf600a550dbcec722081e11c970018ce
luethsil <luethsil@students.zhaw.ch>
2019-11-09 14:51:35 +0100

    add constructor to Processor, add methode printWelcomeText

--- src/main/ch/zhaw/einhoerner/editor/Processor.java
+++ src/main/ch/zhaw/einhoerner/editor/Processor.java
@@ -1,4 +1,24 @@
 package ch.zhaw.einhoerner.editor;
 
 public class Processor {
+
+    public Processor()
+    {
+        //todo maybe do something here
+    }
+
+
+    public void printWelcomeText()
+    {
+        System.out.println();
+        System.out.println("Welcome to the Editor Application from the team Einhoerner");
+        System.out.println("Type in " + EnumCommand.HELP + " at any time for a short manual.");
+        System.out.println();
+        System.out.println("Please use one of the following comands to proceed:");
+        
+    }
+
+
+
 }
+
