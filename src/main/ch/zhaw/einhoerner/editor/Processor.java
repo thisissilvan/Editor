8a8a1942d9e6e3b95613d234279719ffe8127f52
Kunsang Kundetsang <kuendkun@students.zhaw.ch>
2019-11-11 20:59:13 +0100

    Class: Processor
    edit method formatParagraphWidth

--- src/main/ch/zhaw/einhoerner/editor/Processor.java
+++ src/main/ch/zhaw/einhoerner/editor/Processor.java
@@ -144,6 +144,11 @@ public class Processor {
                     seperatorPlace += width + 1;
                     beginningPlace += width + 1;
                     System.out.println(platzhalter);
+                }else {
+                    platzhalter = platzhalter + text.substring(beginningPlace, seperatorPlace) + System.lineSeparator();
+                    seperatorPlace += width;
+                    beginningPlace += width;
+                    System.out.println(platzhalter);
                 }
             }
             paragraphs.set(paragraphIndex, platzhalter);
