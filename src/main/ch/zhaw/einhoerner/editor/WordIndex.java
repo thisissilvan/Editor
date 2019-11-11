1ed3123d4a2156320cb38c44c7dfb3573eb77749
Kunsang Kundetsang <kuendkun@students.zhaw.ch>
2019-11-11 11:03:54 +0100

    Class: Wordindex
    add IllegalArgumentException to method makeWordIndex

--- src/main/ch/zhaw/einhoerner/editor/WordIndex.java
+++ src/main/ch/zhaw/einhoerner/editor/WordIndex.java
@@ -15,51 +15,56 @@ public class WordIndex {
         printWordIndex();
     }
 
-    public void makeWordIndex(List<String> text) {
-        for (int index = 0; index < text.size(); index++) {
-            String platzhalter = text.get(index);
-            String[] paragraph = platzhalter.toLowerCase().replaceAll("[ +.,?!\":; ]", " ").split(" +");
-
-            for (String word : paragraph) {
-                List<String> values = new ArrayList<>();
-                if (!woerterHaeufigkeit.containsKey(word)) {
-                    values.add(AMOUNT, "1");
-                    values.add(PLACE, String.valueOf(index));
-                    woerterHaeufigkeit.put(word, values);
-                } else {
-                    values = woerterHaeufigkeit.get(word);
-                    int raiseCount = Integer.parseInt(values.get(AMOUNT));
-                    raiseCount++;
-                    values.set(AMOUNT, String.valueOf(raiseCount));
-                    String newIndex = String.valueOf(index);
-                    if (!values.get(PLACE).contains(newIndex)) {
-                        String addPlace = values.get(PLACE);
-                        addPlace = addPlace + ", " + newIndex;
-                        values.set(PLACE, addPlace);
-                    }
-                    woerterHaeufigkeit.replace(word, values);
-                }
-
-            }
+    public void makeWordIndex(List<String> text) throws IllegalArgumentException{
+        if (text.size()<=0){
+            throw new IllegalArgumentException ("No text to create a wordindex. Please add text.");
         }
-        entrysToDelete(controlMinimumAmount());
+        if(text.size()>0) {
+            for (int index = 0; index < text.size(); index++) {
+                String platzhalter = text.get(index);
+                String[] paragraph = platzhalter.toLowerCase().replaceAll("[ +.,?!\":; ]", " ").split(" +");
+
+                for (String word : paragraph) {
+                    List<String> values = new ArrayList<>();
+                    if (!woerterHaeufigkeit.containsKey(word)) {
+                        values.add(AMOUNT, "1");
+                        values.add(PLACE, String.valueOf(index));
+                        woerterHaeufigkeit.put(word, values);
+                    } else {
+                        values = woerterHaeufigkeit.get(word);
+                        int raiseCount = Integer.parseInt(values.get(AMOUNT));
+                        raiseCount++;
+                        values.set(AMOUNT, String.valueOf(raiseCount));
+                        String newIndex = String.valueOf(index);
+                        if (!values.get(PLACE).contains(newIndex)) {
+                            String addPlace = values.get(PLACE);
+                            addPlace = addPlace + ", " + newIndex;
+                            values.set(PLACE, addPlace);
+                        }
+                        woerterHaeufigkeit.replace(word, values);
+                    }
+
+                }
+            }
+            entrysToDelete(controlMinimumAmount());
+        }
     }
 
-    private List<String> controlMinimumAmount(){
+    private List<String> controlMinimumAmount() {
         List<String> values;
         List<String> toDelete = new ArrayList<>();
         for (Map.Entry<String, List<String>> wortHaeufigkeit : woerterHaeufigkeit.entrySet()) {
             values = wortHaeufigkeit.getValue();
             int wordCount = Integer.parseInt(values.get(AMOUNT));
-            if (wordCount<3){
+            if (wordCount < 3) {
                 toDelete.add(wortHaeufigkeit.getKey());
             }
         }
         return toDelete;
     }
 
-    private void entrysToDelete(List<String> toDelete){
-        for (String keyToDelete: toDelete) {
+    private void entrysToDelete(List<String> toDelete) {
+        for (String keyToDelete : toDelete) {
             woerterHaeufigkeit.remove(keyToDelete);
         }
 
