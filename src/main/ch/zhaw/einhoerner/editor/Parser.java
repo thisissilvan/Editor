c1b472d6b343a59956c0274c98c5b3d2e3a734f4
hadiala1 <hadiala1@students.zhaw.ch>
2019-11-11 19:17:45 +0100

    Add Javadoc for the extractParameter method

--- src/main/ch/zhaw/einhoerner/editor/Parser.java
+++ src/main/ch/zhaw/einhoerner/editor/Parser.java
@@ -19,12 +19,12 @@ public class Parser {
     public ParsedInput parseInput(String input) {
         // extract command
         Command command = extractCommand(input);
-        if(Command.UNKNOWN.equals(command)) {
+        if (Command.UNKNOWN.equals(command)) {
             return new ParsedInput(input, command, new ArrayList<>());
         }
 
         // if the input consists only of the command, don't parse parameters
-        if( input.length() == command.getCommand().length()) {
+        if (input.length() == command.getCommand().length()) {
             return new ParsedInput(input, command, new ArrayList<>());
         }
 
@@ -38,6 +38,18 @@ public class Parser {
         return new ParsedInput(text, command, parameters);
     }
 
+    /**
+     * This method extracts a command's parameters from the user's input, if there are any.
+     * - The algorithm first removes the detected command within the user's input (e.g. PRINT)
+     * - Next the algorithm detects the command's expected parameter count N (e.g. N=1) and
+     *      removes N words from the beginning of the user's input. Each word will be added to
+     *      list of parameters for the detected command
+     * - the list of commands will now be returned
+     *
+     * @param input   The user's input consisting of a command, potential parameters of this command and an optional text body
+     * @param command The detected command in the user's input
+     * @return List<String> A List<String> of commands
+     */
     private List<String> extractParameter(String input, Command command) {
         List<String> result = new ArrayList<>();
         String text = input.substring(command.getCommand().length() + 1);
@@ -67,14 +79,14 @@ public class Parser {
     /**
      * Extracts the text content without the command
      *
-     * @param input User input with command and text
+     * @param input      User input with command and text
      * @param parameters List of parameters for the command
      * @return String Only the text without command will be returned, if no command can  be found,
      * empty String will be returned
      */
     public String extractText(String input, Command command, List<String> parameters) {
         int index = command.getCommand().length() + 1;
-        for( String parameter: parameters ) {
+        for (String parameter : parameters) {
             index += parameter.length() + 1;
         }
 
