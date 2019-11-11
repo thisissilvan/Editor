45435d6a60a3aa9f3d072f83ce7578a3c3e3f103
luethsil <luethsil@students.zhaw.ch>
2019-11-11 16:45:33 +0100

    add testHelpCommand

--- src/test/ch/zhaw/einhoerner/editor/CommandTest.java
+++ src/test/ch/zhaw/einhoerner/editor/CommandTest.java
@@ -3,31 +3,35 @@ package ch.zhaw.einhoerner.editor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommandTest {
 
-    /**
-     * Standard Constructor for class CommandTest.
-     */
-    public CommandTest()
-    {
-    }
 
     @BeforeEach
-    public void setUp()
+    void setUp()
     {
     }
 
     @AfterEach
-    public void tearDown()
+    void tearDown()
     {
     }
 
     @Test
-    public void getCommand()
+    public void testHelpCommand()
     {
+        Command command = Command.HELP;
+        String helpCommand = command.HELP.getCommand();
+        int parameterCountHelp = command.HELP.getParameterCount();
 
+        assertNotNull(command);
+        assertNotEquals(Command.valueOf("UNKNOWN"), command);
+        assertEquals(Command.valueOf("HELP"), command);
+        assertEquals("help", helpCommand);
+        assertEquals(0, parameterCountHelp);
     }
+
+
+
 }
