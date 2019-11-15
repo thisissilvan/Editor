b666a1d6c7e81f23bcaaae086b5099762176b177
Kunsang Kundetsang <kuendkun@students.zhaw.ch>
2019-11-15 10:42:18 +0100

    Class ProcessorTest
    Tests made for method printFormattedWidth

--- src/test/ch/zhaw/einhoerner/editor/ProcessorTest.java
+++ src/test/ch/zhaw/einhoerner/editor/ProcessorTest.java
@@ -3,6 +3,9 @@ package ch.zhaw.einhoerner.editor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.lang.System.lineSeparator;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -71,8 +74,33 @@ class ProcessorTest {
         p.executeCommand(parsedInput);
         assertThat("Processor should have 5 paragraphs after adding the example text.", 5, is(p.getParagraphs().size()));
     }
+    @Test
+    public void formatParagraphWidthWithItem(){
+        processor.add("a b c d e f g, h i");
+        List<String> test = new ArrayList<>();
+        test.add("a b c d e f g, "+System.lineSeparator()+ "h i ");
+        assertLinesMatch(test,processor.formatParagraphWidth(15));
+    }
 
+    @Test
+    public void formatParagraphWidthWithoutItem(){
+        List<String> test = new ArrayList<>();
+        assertLinesMatch(test,processor.formatParagraphWidth(15));
+    }
 
+    @Test
+    public void formatParagraphWidthNegativeWidth(){
+        processor.add("a b c d e f g, h i");
+        List<String> test = new ArrayList<>();
+        assertLinesMatch(test,processor.formatParagraphWidth(-15));
+    }
+
+    @Test
+    public void formatParagraphWidthTooLongWidth(){
+        processor.add("a b c d e f g, h i");
+        List<String> test = new ArrayList<>();
+        assertLinesMatch(test,processor.formatParagraphWidth(100));
+    }
 
 
 }
